fc0a895d8fed3c604da484438af3508e31e11410
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-09-30 07:50:16 +0200

    Add example

---
+++ example/generate-token.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+/* eslint-disable no-console */
+
+const GoogleOAuth2 = require("../")
+
+async function generateToken() {
+  const googleOAuth2 = new GoogleOAuth2({
+    scope: [
+      "https://www.googleapis.com/auth/documents.readonly",
+      "https://www.googleapis.com/auth/drive.metadata.readonly",
+    ],
+    token: "GOOGLE_DOCS_TOKEN",
+    apis: ["docs.googleapis.com", "drive.googleapis.com"],
+  })
+
+  await googleOAuth2.generateEnvVars()
+
+  process.exit()
+}
+
+generateToken()
